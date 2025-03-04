c7536129d4762996b23a30316cc1ba029030710a
Aradhyaaaaaaaa <aradhya.trivedi@coditas.com>
2025-03-04 10:17:45 +0530

    main.go

--- main.go
+++ main.go
@@ -1,38 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+    "fmt"
+    "sync"
 )
 
-func main(){
-	fmt.Println("SQUARE NUMBERS")
-	list := []int{1,2,3,4,5,6,7,8,9}
+func main() {
+    fmt.Println("SQUARE NUMBERS")
+    list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-	results := make(chan int)
-	var wg sync.WaitGroup
+    results := make(chan int, len(list))
+    var wg sync.WaitGroup
 
-	go aggregate(results, &wg)
+    // Process squares
+    for _, num := range list {
+        wg.Add(1)
+        go square(num, results, &wg)
+    }
 
-	for _, num := range list{
-		wg.Add(1)
-		go square(num, results, &wg)
-	}
-	wg.Wait()
-	close(results)
-		
-	}
-func square(num int, results chan int, wg *sync.WaitGroup ){
-	square := num*num
-	results <- square
-	defer wg.Done() //goroutine done
+    // Close the channel after all squares are done
+    go func() {
+        wg.Wait()       
+        close(results)  
+    }()
+
+    // Start the aggregator
+    aggregate(results) // Run aggregate in the main goroutine
+
+    // fmt.Println("Main function exiting")
 }
-func aggregate(results chan int, wg *sync.WaitGroup){
-	sum:= 0
-	for squarenumber := range results{
-		sum+= squarenumber
-	}
-	fmt.Print("Aggregate of a square function is", sum)
-	defer wg.Done()
 
+func square(num int, results chan int, wg *sync.WaitGroup) {
+    defer wg.Done()
+    square := num * num
+    results <- square
+}
+
+func aggregate(results chan int) {
+    sum := 0
+    fmt.Printf("Hello\n")
+    for num := range results {
+        sum += num
+    }
+    fmt.Printf("Aggregate of square function is %d\n", sum)
 }
