6c747ec6eace54e3fa81f9be2f6b4fab88e3c276
Aradhyaaaaaaaa <aradhya.trivedi@coditas.com>
2025-03-04 14:25:50 +0530

    Goroutine Assignment done

--- main.go
+++ main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "sync"
+	"fmt"
+	"sync"
 )
+
 func main() {
-    fmt.Println("SQUARE NUMBERS")
-    list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println("SQUARE NUMBERS")
+	list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-    results := make(chan int, len(list))
-    var wg sync.WaitGroup
+	results := make(chan int, len(list))
+	var wg sync.WaitGroup
 
-    //for squares
-    for _, num := range list {
-        wg.Add(1)
-        go square(num, results, &wg)
-    }
+	//for squares
+	for _, num := range list {
+		wg.Add(1)
+		go square(num, results, &wg)
+	}
 
-    // Close the channel after all squares are done
-    go func() {
-        wg.Wait()       
-        close(results)  
-    }()
+	// Close the channel after all squares are done
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
 
-    // Start the aggregator
+	// Start the aggregator
 	var wga sync.WaitGroup
 	wga.Add(1)
 	go aggregate(results, &wga)
@@ -32,18 +33,18 @@ func main() {
 }
 
 func square(num int, results chan int, wg *sync.WaitGroup) {
-    defer wg.Done()
-    square := num * num
-    results <- square
+	defer wg.Done()
+	square := num * num
+	results <- square
 }
 
-func aggregate(results chan int, wga* sync.WaitGroup) {
-    defer wga.Done()
+func aggregate(results chan int, wga *sync.WaitGroup) {
+	defer wga.Done()
 	sum := 0
-
-    fmt.Printf("Hello\n")
-    for num := range results {
-        sum += num
-    }
-    fmt.Printf("Aggregate of square function is %d\n", sum)
+	fmt.Printf("Hello\n")
+	for num := range results {
+		sum += num
+	}
+    
+	fmt.Printf("Aggregate of square function is %d\n", sum)
 }
